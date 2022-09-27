832f1256df3ee16073532d895c27dd7a2079e6e6
hugogdiniz <hugogdiniz.ti@gmail.com>
2022-09-27 13:44:43 -0300

    implementando JPA na modal Pedido

--- src/main/java/br/me/desafio/backendchallenge/entities/Pedido.java
+++ src/main/java/br/me/desafio/backendchallenge/entities/Pedido.java
@@ -1,9 +1,18 @@
 package br.me.desafio.backendchallenge.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@Entity
 public class Pedido implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
 
