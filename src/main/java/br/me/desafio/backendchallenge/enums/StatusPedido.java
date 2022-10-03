162b4f9afbe4ed7328487f55c4e59d24127debac
hugogdiniz <hugogdiniz.ti@gmail.com>
2022-10-03 13:43:44 -0300

    Adicionando enums

---
+++ src/main/java/br/me/desafio/backendchallenge/enums/StatusPedido.java
@@ -0,0 +1,14 @@
+package br.me.desafio.backendchallenge.enums;
+
+public enum StatusPedido {
+
+    APROVADO,
+    APROVADO_VALOR_A_MENOR,
+    APROVADO_VALOR_A_MAIOR,
+    APROVADO_QTD_A_MAIOR,
+    APROVADO_QTD_A_MENOR,
+    REPROVADO,
+    CODIGO_PEDIDO_INVALIDO;
+
+
+}
