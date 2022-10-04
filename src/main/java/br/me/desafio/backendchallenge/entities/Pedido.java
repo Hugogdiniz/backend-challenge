a15b36880348e5c492bd63bdbce14b4c30192f06
hugogdiniz <hugogdiniz.ti@gmail.com>
2022-10-04 09:38:36 -0300

    id do pedido agora é inserido manualmente

--- src/main/java/br/me/desafio/backendchallenge/entities/Pedido.java
+++ src/main/java/br/me/desafio/backendchallenge/entities/Pedido.java
@@ -14,7 +14,7 @@ public class Pedido implements Serializable {
 
     @Id
     @Column(name = "ID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
 
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
